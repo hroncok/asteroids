04a21ec2e27bf6a42301ab2ca4f5ff94934d6d08
Miro Hrončok <miro@hroncok.cz>
2017-12-18 19:58:35 +0100

    Bonus 2 from the SpaceShip section

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import math
 import random
 
 import pyglet
+from pyglet import gl
 from pyglet.window import key
 
 # Consts
@@ -141,7 +142,19 @@ for _ in range(8):
 def draw_all_objects():
     """For all objects, draw theirs sprites"""
     window.clear()
-    batch.draw()
+
+    for x_offset in (-window.width, 0, window.width):
+        for y_offset in (-window.height, 0, window.height):
+            # Remember the current state
+            gl.glPushMatrix()
+            # Move everything drawn from now on by (x_offset, y_offset, 0)
+            gl.glTranslatef(x_offset, y_offset, 0)
+
+            # Draw
+            batch.draw()
+
+            # Restore remembered state (this cancels the glTranslatef)
+            gl.glPopMatrix()
 
 
 def tick_all_objects(dt):
